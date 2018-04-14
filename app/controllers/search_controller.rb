ce8aeff58a72fafd29075707c46abf8471e4c4c4
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2018-04-15 01:06:43 +0530

    fix search query-a

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     if params[:query].present?
       @current_user_friend_ids = current_user.friend_ids
-      @users = User.where("name like '%#{params[:query]}%' OR email = '%#{params[:query]}%'").where.not(id: current_user.id).includes(:friends)
+      @users = User.where("name like '%#{params[:query]}%' OR email like '%#{params[:query]}%'").where.not(id: current_user.id).includes(:friends)
     else
       @users = []
     end
