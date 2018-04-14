bb2815ea4539fcd227158bf6a3d4bce2a31774eb
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2018-04-14 17:10:07 +0530

    location focus set to kolkata

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -2,22 +2,27 @@ var LS = LS || {};
 
 LS.Map = {
   initMap: function () {
+    var kolkata = ol.proj.fromLonLat([88.3639, 22.5726]);
+    var view = new ol.View({
+      center: kolkata,
+      zoom: 8
+    });
+
+    var layers = [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ];
+
     var map = new ol.Map({
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
+      layers: layers,
       target: 'map',
       controls: ol.control.defaults({
         attributionOptions: {
           collapsible: false
         }
       }),
-      view: new ol.View({
-        center: [0, 0],
-        zoom: 2
-      })
+      view: view
     });
     
   }
